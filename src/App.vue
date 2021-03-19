0f0e4bd13f4b24e6d7b956cc2a917b7783080f6c
wggreen <wggreen.nss@gmail.com>
2021-03-19 11:44:05 -0500

    done with part 2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-main class="pa-6 blue-grey lighten-4">
+      <v-row justify="center">
+        <v-col :cols="12" :md="8" :lg="4">
+          <annual-income
+            :annualAmount="annualIncome"
+            @income-change="handleIncomeChange"
+          />
+        </v-col>
+        <v-col :cols="12" :md="6" :lg="4">
+          <income title="Monthly Net" :net-amount="monthlyNet" />
+        </v-col>
+        <v-col :cols="12" :md="6" :lg="4">
+          <income title="Annual Net" :net-amount="annualNet" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col :cols="12" :lg="6">
+          <expenses
+            :expenses="monthlyExpenses"
+            @expense-added="handleExpenseAdded"
+            @expense-removed="handleExpenseRemoved"
+          />
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AnnualIncome from "./components/AnnualIncome";
+import Expenses from "./components/Expenses";
+import Income from "./components/Income";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  components: { AnnualIncome, Expenses, Income },
+  data() {
+    return {
+      annualIncome: 0,
+      monthlyExpenses: [],
+    };
+  },
+  computed: {
+    monthlyIncome() {
+      return this.annualIncome / 12;
+    },
+    totalMonthlyExpenses() {
+      return this.monthlyExpenses.reduce((total, month) => {
+        return total + month.amount;
+      }, 0);
+    },
+    annualExpenses() {
+      return this.totalMonthlyExpenses * 12;
+    },
+    monthlyNet() {
+      return (this.monthlyIncome - this.totalMonthlyExpenses).toFixed(2);
+    },
+    annualNet() {
+      return +(this.annualIncome - this.annualExpenses).toFixed(2);
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/AnnualIncome.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card>
+    <v-card-title class="purple--text">Annual Income</v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          v-model="inputVal"
+          type="number"
+          outlined
+          color="purple"
+          filled
+          prefix="$"
+        />
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["annualAmount"],
+};
+</script>
+
+<style>
+</style>
